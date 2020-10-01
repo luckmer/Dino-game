4a77d73b1ebdf4018184f98943f03e0471646768
luckmer <woszczo04@gmail.com>
2020-10-01 10:42:57 +0200

    generate block

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const model = document.querySelector(".character");
     const grid = document.querySelector(".grid");
-    let bottom = 0;
-    let gravity = 0.9;
+    let bottom = 0,
+        gravity = 0.9,
+        stop = false;
+
     function jump() {
-        let upTimerId = setInterval(function () {
+        let jumpUp = setInterval(function () {
             if (bottom > 250) {
-                clearInterval(upTimerId);
-                let downTimerId = setInterval(function () {
+                clearInterval(jumpUp);
+                let jumpDown = setInterval(function () {
                     if (bottom < 0) {
-                        clearInterval(downTimerId);
+                        clearInterval(jumpDown);
                     }
                     bottom -= 5;
                     bottom *= gravity;
@@ -22,11 +24,37 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 20);
     }
 
+    function createBlock() {
+        let left = grid.clientWidth;
+        const block = document.createElement("div");
+        block.classList.add("block");
+        grid.appendChild(block);
+        block.style.left = left + "px";
+        function moveBlock() {
+            left -= 2;
+            if (left < -50) {
+                clearInterval(timerId);
+                grid.removeChild(block);
+            }
+            if (left > 0 && left < 40 && bottom < 0) {
+                stop = true;
+                clearInterval(timerId);
+                grid.removeChild(block);
+                alert("game over");
+                location.reload();
+            }
+            block.style.left = left + "px";
+        }
+        let timerId = setInterval(moveBlock, 2);
+        if (!stop) setTimeout(createBlock, 2000);
+    }
+
     function control(e) {
         if (e.keyCode == 32) {
             jump();
         }
     }
+    createBlock();
 
     document.addEventListener("keyup", control);
 });
